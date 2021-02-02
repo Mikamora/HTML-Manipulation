6765bd5179c54b7eb3e3a29e7749f2ed588e2dc0
Michael Richardson <mor7991@yahoo.com>
2021-02-02 15:13:23 -0600

    finished

---
+++ DOM/app.js
@@ -0,0 +1,63 @@
+document.addEventListener("DOMContentLoaded", function(){
+    // 1
+    let btn = document.createElement("button");
+    let btnText = document.createTextNode("click!");
+    btn.appendChild(btnText);
+    document.body.appendChild(btn);
+    btn.addEventListener("click", function(){
+        alert("you fell for that?!?!");
+    })
+    // 2
+    let btn2 = document.querySelector("#btn2");
+    btn2.addEventListener("click", function(){
+        let text = document.querySelector("#text1");
+        alert(text.value);
+    })
+    // 3
+    let div1 = document.querySelector(".hover");
+    div1.addEventListener("mouseenter", function(){
+        var colorR = Math.floor((Math.random() * 256));
+        var colorG = Math.floor((Math.random() * 256));
+        var colorB = Math.floor((Math.random() * 256));
+        div1.style.background = `rgb(${colorR}, ${colorG}, ${colorB})`
+    })
+    div1.addEventListener("mouseleave", function(){
+        div1.style.background = "transparent";
+    })
+    // 4
+    let para = document.createElement("p");
+    let paraText = document.createTextNode("click me!");
+    para.appendChild(paraText);
+    document.body.appendChild(para);
+    para.addEventListener("click", function(){
+        var colorR = Math.floor((Math.random() * 256));
+        var colorG = Math.floor((Math.random() * 256));
+        var colorB = Math.floor((Math.random() * 256));
+        para.style.color = `rgb(${colorR}, ${colorG}, ${colorB})`
+    })
+    // 5
+    let btn3 = document.createElement("button");
+    btn3.textContent = "click me!";
+    let div2 = document.createElement("div");
+    document.body.appendChild(btn3);
+    document.body.appendChild(div2);
+    btn3.addEventListener("click", function(){
+        let newSpan = document.createElement("span");
+        let newSpanText = document.createTextNode("Michael ");
+        newSpan.appendChild(newSpanText);
+        div2.appendChild(newSpan);
+    })
+    // 6
+    let arrOfFriends = ["john", 'jacob', 'justin', 'jordan', 'kasandra', 'matt', 'rachel', 'alex', 'josh', 'samantha'];
+    let btn4 = document.getElementById("btn4");
+    let ul = document.querySelector("ul");
+    btn4.addEventListener("click", function(){
+        for (let i = 0; i < arrOfFriends.length; i++) {
+            let li = document.createElement("li");
+            let liText = document.createTextNode(arrOfFriends[i]);
+            li.appendChild(liText);
+            ul.appendChild(li);
+        }
+    })
+
+})
---
+++ JQuery/app.js
@@ -0,0 +1,57 @@
+// 1
+$(document).ready(function(){
+    let $btn = $("<button>Click me!</button>");
+    $("body").append($btn);
+    $btn.click(function(){
+        alert("hey!! STOP THAT!")
+    })
+    // 2
+    let $btn1 = $("#btn1");
+    let $textBox = $("#text");
+    $btn1.click(function(){
+        let $textBoxVal = $textBox.val();
+        alert($textBoxVal);
+        event.preventDefault();
+    })
+    // 3
+    let $div1 = $(".div1");
+    $div1.mouseenter(function(){
+        var colorR = Math.floor((Math.random() * 256));
+        var colorG = Math.floor((Math.random() * 256));
+        var colorB = Math.floor((Math.random() * 256));
+        $(this).css("background-color", `rgb(${colorR}, ${colorG}, ${colorB})`);
+    })
+    $div1.mouseleave(function(){
+        $(this).css("background-color", "transparent");
+    })
+    // 4
+    let $p = $("<p> hello!! </p>");
+    $("body").append($p);
+    $p.click(function(){
+        var colorR = Math.floor((Math.random() * 256));
+        var colorG = Math.floor((Math.random() * 256));
+        var colorB = Math.floor((Math.random() * 256));
+        $(this).css("color", `rgb(${colorR}, ${colorG}, ${colorB})`);
+    })
+    // 5
+    let $btn2 = $("<button>Click me!!!!!</button>");
+    let $div2 = $("<div></div>");
+    $("body").append($btn2);
+    $("body").append($div2);
+    $btn2.click(function(){
+        let $span = $("<span>Michael! </span>");
+        $div2.append($span);
+    })
+    // 6
+    let friends = ["john", 'jacob', 'justin', 'jordan', 'kasandra', 'matt', 'rachel', 'alex', 'josh', 'samantha'];
+    let $btn3 = $(".friends");
+    let $ul = $("ul");
+    $btn3.click(function(){
+        for (let i = 0; i < friends.length; i++) {
+            let $li = $("<li>" + friends[i] + "</li>");  
+            $ul.append($li);
+        }
+    })
+
+
+})
